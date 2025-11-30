61ec7667cc1ad3c56cc28de43103c0b1ba329530
Denis-qa5 <digorev555@gmail.com>
2025-11-30 15:26:40 +0300

    Update Main.java

--- src/main/java/ru/TESTOOK/Main.java
+++ src/main/java/ru/TESTOOK/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and РАСДАВА!");
+        System.out.printf("Hello and РАСДАВА ПОРА В!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
